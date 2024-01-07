484cc784a9debd1d5407c37a6b9a8c942290de15
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2024-01-07 23:33:51 +0300

    ABStore

---
+++ script.js
@@ -0,0 +1,266 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const wrapper = document.querySelector(".sliderWrapper");
+    const menuItem = document.querySelectorAll(".menuItem");
+
+    //wrapper.style.transform = translateX(-400vw); every -100vw display the next item 
+
+    const products = [
+        {
+            id: 1,
+            title: "Sweater",
+            price: 119,
+            colors: [
+                {
+                    code: "black",
+                    img: "./img/sweater.png",
+                },
+                {
+                    code: "#FFC47E",
+                    img: "./img/sweater2.png",
+                },
+            ],
+            sizes: ["M", "L", "XL"]
+        },
+
+        {
+            id: 2,
+            title: "Backpack",
+            price: 149,
+            colors: [
+                {
+                    code: "lightgray",
+                    img: "./img/backpack.png",
+                },
+                {
+                    code: "#186F65",
+                    img: "./img/backpack2.png",
+                },
+            ],
+            sizes: ["30L", "50L", "80L"]
+        },
+        {
+            id: 3,
+            title: "Blazer",
+            price: 109,
+            colors: [
+                {
+                    code: "lightgray",
+                    img: "./img/blazer.png",
+                },
+                {
+                    code: "green",
+                    img: "./img/blazer2.png",
+                },
+            ],
+            sizes: [42, 43, 44]
+        },
+        {
+            id: 4,
+            title: "Jacket",
+            price: 129,
+            colors: [
+                {
+                    code: "#F4BBBB",
+                    img: "./img/jacket.png",
+                },
+                {
+                    code: "black",
+                    img: "./img/jacket2.png",
+                },
+            ],
+            sizes: ["M", "L", "XL"]
+        },
+        {
+            id: 5,
+            title: "t-shirt",
+            price: 99,
+            colors: [
+                {
+                    code: "#DBCC95",
+                    img: "./img/t-shirt2.png",
+                },
+                {
+                    code: "#A8A196",
+                    img: "./img/t-shirt.png",
+                },
+            ],
+            sizes: ["S", "M", "L"]
+        },
+    ];
+
+    const addToCart = document.querySelector(".addToCart");
+
+    //notification of the normal navbar
+    var currentShippedItem = document.querySelector(".numberOfItem");
+
+    //notification of the sidebar 
+    var shippedItemNo = document.querySelector(".numberOfItem2");
+
+    let productButtonArray = [];
+    let productButtonData = {}; // Define productButtonData in the outer scope
+
+
+    //retrieving data if any
+    const storedArray = localStorage.getItem("productButtonArray");
+
+    if(storedArray){
+        productButtonArray = JSON.parse(storedArray);
+        console.log(productButtonArray);
+        if(productButtonArray.length != 0){
+
+            if(window.innerWidth <= 1052){
+                // for adding the notification of the cart  
+                shippedItemNo.style.display = "flex";
+                shippedItemNo.innerHTML = productButtonArray.length;
+            }else{
+                // controlling the notification of the item that being shipped
+                currentShippedItem.style.display = "block"
+                currentShippedItem.innerHTML = productButtonArray.length;
+            }
+        }
+    }
+    
+
+
+    
+
+    const currentProductImg = document.querySelector(".productImg");
+    const currentProductTitle = document.querySelector(".productTitle");
+    const currentProductPrice = document.querySelector(".productPrice");
+    const currentProductColors = document.querySelectorAll(".color");
+    const currentProductSizes = document.querySelectorAll(".size");
+
+    // controlling the flow of the chosenProduct and indexOfColor as a default reference
+    var chosenProduct = products[0];
+    var indexOfColor = 0;
+
+    
+
+    menuItem.forEach((item, index) =>{
+
+        item.addEventListener("click", () => {
+            // control the index of the menuitem if the navTopMobile is being called the index might differ
+            if(index >= products.length){
+                index -= products.length;
+            }
+            
+            //change the current slide
+            wrapper.style.transform = `translateX(${-100 * index}vw)`; // Use backticks for proper string interpolation
+
+            //resetting the indexOfColor to be sit as 0 when the item get loaded first
+            indexOfColor = 0;
+
+            //change the chosen product
+            chosenProduct = products[index];
+
+            //change text of current product 
+            currentProductTitle.textContent = chosenProduct.title;
+
+            currentProductPrice.textContent = "$" + String(chosenProduct.price);
+
+            currentProductImg.src = chosenProduct.colors[0].img;
+
+            //assign new color
+            currentProductColors.forEach((color, index) => {
+                color.style.backgroundColor = chosenProduct.colors[index].code;
+            });
+
+            //assigning sizes based on the item type
+            document.querySelector(".size1").innerHTML = chosenProduct.sizes[0];
+            document.querySelector(".size2").innerHTML = chosenProduct.sizes[1];
+            document.querySelector(".size3").innerHTML = chosenProduct.sizes[2];
+            
+        });
+
+    });
+
+    
+    //color of the item
+    currentProductColors.forEach((color, index) => {
+        color.addEventListener("click", ()=>{
+            currentProductImg.src = chosenProduct.colors[index].img;
+            indexOfColor = index;
+        });
+    });
+
+
+    addToCart.addEventListener("click", function(){
+        productButtonData = {
+            imgSrc: chosenProduct.colors[indexOfColor].img,
+            title: chosenProduct.title,
+            detail: 'loremkjbf fjknagl kkgnal',
+            seller: 'sneaker',
+            warranty: '30 days warranty',
+            price: chosenProduct.price
+        };
+        productButtonArray.push(productButtonData);
+        console.log(productButtonArray);
+
+        if(window.innerWidth <= 1052){
+            shippedItemNo.style.display = "flex";
+            shippedItemNo.innerHTML = productButtonArray.length;
+        }else{
+            // controlling the notification of the item that being shipped
+            currentShippedItem.style.display = "block"
+            currentShippedItem.innerHTML = productButtonArray.length;
+        }
+
+        localStorage.setItem('productButtonArray', JSON.stringify(productButtonArray));
+    });
+
+    currentProductSizes.forEach((size, index)=>{
+        size.addEventListener("click", ()=>{
+            // before changing the button to bg black and color white it will set all product back to the default color AND THEN change the intended size color
+            currentProductSizes.forEach((size)=>{
+                size.style.backgroundColor = "white";
+                size.style.color = "black";
+            });
+
+            size.style.backgroundColor = "black";
+            size.style.color = "white";
+        });
+    });
+
+    //constrolling the flow of the navTopMobile
+
+    const menuIcon = document.querySelector(".menuIcon");
+    const crossIcon = document.querySelector(".crossIcon");
+    const navTopMobile = document.querySelector(".navTopMobile");
+
+    menuIcon.addEventListener("click", function(){
+        navTopMobile.style.display = "flex";
+
+        // adding the notification of items if exists
+        shippedItemNo.style.display = "flex";
+        shippedItemNo.innerHTML = productButtonArray.length;
+    });
+
+    crossIcon.addEventListener("click", function(){
+        navTopMobile.style.display = "none";
+    });
+
+    //holder of the contact form & overlay
+    const contactForm = document.querySelector(".contact");
+    const overlay = document.querySelector('.overlay');
+
+    //controlling the contact pop out
+    const contactPopOut = document.querySelector(".fContact");
+    
+    contactPopOut.addEventListener("click", function(){
+        contactForm.style.display = "flex";
+        contactForm.classList.remove("hide");
+        contactForm.classList.add("show");
+        overlay.style.display = 'block'; // Show the overlay when contact form is displayed
+    })
+
+    //controlling the cross of the contact form to close the pop out
+    const closeContactPopOut = document.querySelector(".close-contact-icon");
+
+    closeContactPopOut.addEventListener("click", function(){
+        contactForm.classList.remove("show");
+        contactForm.classList.add("hide");
+        overlay.style.display = 'none'; // Hide the overlay when contact form is closed
+    })
+
+});
---
+++ script2.js
@@ -0,0 +1,213 @@
+const storedArray = localStorage.getItem("productButtonArray");
+const parsedArray = JSON.parse(storedArray);
+
+console.log(parsedArray);
+
+const emptyCart = document.querySelector(".emptyCart");
+const psTitle = document.querySelector(".psTitle");
+const psPrice = document.querySelector(".psPrice");
+const totalPrice = document.querySelector(".tpNumber");
+
+let totalItemPrice = 0
+
+//to display the block of the empty cart 
+if(parsedArray.length === 0){
+    emptyCart.style.display = "flex";
+}
+
+//checking for the existing of the array
+if(storedArray){
+    console.log(parsedArray);
+    for(var i = 0; i < parsedArray.length; i++){
+        addShippedItem(parsedArray[i].imgSrc, parsedArray[i].title, parsedArray[i].detail, parsedArray[i].seller, parsedArray[i].warranty);
+
+        totalItemPrice += parsedArray[i].price;
+    }
+
+    //setting the price of the item
+    psTitle.innerHTML = "Subtotal (" + parsedArray.length+ " item)";
+    psPrice.innerHTML = "USD " + totalItemPrice.toFixed(2);
+    totalPrice.innerHTML = "USD " + totalItemPrice.toFixed(2);
+}else{
+    console.log('array doesn\'t exist');
+}
+
+
+// const retrieve = require("./script");
+// console.log(retrieve.productButtonArray);
+
+//creating new element in this cart.html
+function addShippedItem(imgSrc, title, detail, seller, warranty) {
+    const shippingSlider = document.querySelector(".shippingSlider");
+
+    // Create elements for the new shipped item
+    const newShippedItem = document.createElement("div");
+    newShippedItem.classList.add("shippedItem");
+
+    const img = document.createElement("img");
+    img.src = imgSrc;
+    img.alt = "";
+    img.classList.add("itemImg");
+
+    const itemDesc = document.createElement("div");
+    itemDesc.classList.add("itemDesc");
+
+    const itemTitle = document.createElement("span");
+    itemTitle.classList.add("itemTitle");
+    itemTitle.textContent = title;
+
+    const itemDetails = document.createElement("span");
+    itemDetails.classList.add("itemDetails");
+    itemDetails.textContent = detail;
+
+    const sellerName = document.createElement("span");
+    sellerName.classList.add("sellerName");
+    sellerName.innerHTML = `sold by <b>${seller}</b>`;
+
+    const itemWarranty = document.createElement('span');
+    itemWarranty.classList.add("warranty");
+    itemWarranty.innerHTML = `<b>${warranty}</b>`;
+
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add("removeBtn");
+
+    const deleteImg = document.createElement("img");
+    deleteImg.src = './img/delete.png';
+    deleteImg.alt = "";
+    deleteImg.classList.add('delete');
+
+    const removeText = document.createTextNode('remove');
+    removeBtn.appendChild(deleteImg);
+    removeBtn.appendChild(removeText);
+
+    itemDesc.appendChild(itemTitle);
+    itemDesc.appendChild(itemDetails);
+    itemDesc.appendChild(sellerName);
+    itemDesc.appendChild(itemWarranty);
+    itemDesc.appendChild(removeBtn);
+
+    newShippedItem.appendChild(img);
+    newShippedItem.appendChild(itemDesc);
+    
+    shippingSlider.appendChild(newShippedItem);
+}
+
+
+/*popping out the payment method*/
+const productPayment = document.querySelector(".checkoutBtn");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+const shippingCart = document.querySelector(".shippingCart");
+const payBtn = document.querySelector(".payButton");
+
+//overlay holder for bluring
+const overlay = document.querySelector('.overlay');
+
+
+
+productPayment.addEventListener("click", function(){
+    payment.style.display = "flex";
+    overlay.style.display = 'block'; // Show the overlay when contact form is displayed
+});
+
+
+close.addEventListener("click", ()=>{
+    payment.style.display = "none";
+    overlay.style.display = 'none'; // Hide the overlay when contact form is closed
+});
+
+payBtn.addEventListener("click", function(){
+    payment.style.display = "none";
+    overlay.style.display = 'none'; // Hide the overlay when contact form is closed
+})
+
+
+
+
+// fixed itemPrice to be visible with scrolling in the top 
+
+// document.addEventListener("DOMContentLoaded", function() {
+
+//     var itemPrice = document.querySelector('.itemPrice');
+//     var initialOffset = itemPrice.getBoundingClientRect().top + window.scrollY;
+
+//     window.addEventListener("scroll", function() {
+//         var scrollTop = window.scrollY || window.pageYOffset; // Account for different browsers
+//         if (scrollTop > initialOffset) {
+//         itemPrice.classList.add('fixed');
+//         } else {
+//         itemPrice.classList.remove('fixed');
+//         }
+//     }); 
+//     window.addEventListener("resize", function() {
+//     initialOffset = itemPrice.getBoundingClientRect().top + window.scrollY; // Recalculate initialOffset on window resize
+//     });
+// });
+
+
+//remove button action for the shippedItem 
+const shippedItem = document.querySelectorAll(".shippedItem");
+
+shippedItem.forEach((item, index) => {
+    item.addEventListener("click", function(e){
+        if(e.target.tagName === "BUTTON"){
+            e.target.parentElement.parentElement.remove();
+
+            //remove the item from the localStorage
+            if(parsedArray.length != 0){
+                totalItemPrice -= parsedArray[index].price;
+                parsedArray.splice(index, 1);
+                console.log(parsedArray);
+
+                localStorage.setItem("productButtonArray", JSON.stringify(parsedArray));
+
+                //resetting the order summary
+                // timeout for the data to be processed at first so we prevent the runtime error
+                // setTimeout(function() {
+                //     localStorage.setItem("productButtonArray", JSON.stringify(parsedArray));
+                // }, 1000);
+                
+                psTitle.innerHTML = "Subtotal (" + parsedArray.length+ " item)";
+                psPrice.innerHTML = "USD " + totalItemPrice.toFixed(2);
+                totalPrice.innerHTML = "USD " + totalItemPrice.toFixed(2);
+
+                
+                //checking for empty cart 
+                if(parsedArray.length === 0){
+                    emptyCart.style.display = "flex";
+                    psTitle.innerHTML = "Subtotal (0 item)";
+                    psPrice.innerHTML = "USD 0.00";
+                    totalPrice.innerHTML = "USD 0.00";  
+                }
+            }
+        }else{
+            console.log("something went wrong");
+        }
+    })
+});
+
+//holder of the contact form
+const contactForm = document.querySelector(".contact");
+
+
+
+//controlling the contact pop out
+const contactPopOut = document.querySelector(".fContact");
+
+contactPopOut.addEventListener("click", function(){
+    contactForm.style.display = "flex";
+    contactForm.classList.remove("hide");
+    contactForm.classList.add("show");
+    overlay.style.display = 'block'; // Show the overlay when contact form is displayed
+})
+
+//controlling the cross of the contact form to close the pop out
+const closeContactPopOut = document.querySelector(".close-contact-icon");
+
+closeContactPopOut.addEventListener("click", function(){
+    contactForm.classList.remove("show");
+    contactForm.classList.add("hide");
+    overlay.style.display = 'none'; // Hide the overlay when contact form is closed
+})
+
+
